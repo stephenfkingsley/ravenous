535030d3a05efa0451378d247513fb2e154d3023
stephenfkingsley <stephenfkingsley0@gmail.com>
2017-11-23 15:51:44 -0600

    Troubleshooting Let's Go Button.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -57,8 +57,9 @@ class SearchBar extends React.Component {
 	}
 
 	handleSearch(event) {
-		this.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
 		event.preventDefault();
+		console.log('this.props', this.props);
 	}
 
    render() {
