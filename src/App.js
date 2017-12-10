5f5adaadcbe32c13406e2aae171a38ca593fb6fe
stephenfkingsley <stephenfkingsley0@gmail.com>
2017-12-09 22:18:51 -0600

    Correcting missing this.state line 27.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     		<div className="App">
     		  <h1>ravenous</h1>
     		   <SearchBar searchYelp={this.searchYelp} />
-    		   <BusinessList businesses={this.businesses} />
+    		   <BusinessList businesses={this.state.businesses} />
     		</div>
     );
   }
